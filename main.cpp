aff2a37ef1db22277faf31bbeaa3308e1536db55
vicente de souza <vicentedesouza762@gmail.com>
2024-10-29 00:24:50 +0000

    comentario

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std
+
+int main (){
+    int idade;
+    cout << "Digite a sua idade; ";
+    cin >> idade;
+    if (idade >= 18){
+        cout << "voce pode ser preso";
+    }
+    else{
+        cout << "voce vai pra FASE";
+    }
+    return 0;
+}
